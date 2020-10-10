dc84732efb3cb51faf6e59577b70f943c8d41286
Caio Fernando <caiofsr13@gmail.com>
2020-10-10 19:01:26 -0300

    indexes all goals

--- app/Controllers/Http/GoalController.js
+++ app/Controllers/Http/GoalController.js
@@ -3,6 +3,12 @@
 const presenter = use('App/Mediators/Goal')
 
 class GoalController {
+  async index({ response }) {
+    const { status, data } = await presenter.Indexes()
+
+    return response.status(status).send(data)
+  }
+
   async store({ request, response, auth }) {
     const user = await auth.getUser()
     const id = user.$attributes.id
